efef0501d682879ae02ee56955bd25da3c282bcf
fredmugambi <fredmugambi7@gmail.com>
2023-03-23 04:20:13 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include<stdio.h>
 #include<unistd.h>
 /**
- *  main - A C program that prints line to standard error
+ *  main - print "and that piece of art is useful" - Dora Korpar, 2015-10-19"
  *  Return 1(Success)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+write (2 ,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
 
