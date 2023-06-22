f3f7d8da8932a946429ec512db58890ac559f970
fredmugambi <fredmugambi7@gmail.com>
2023-06-22 08:08:53 -0700

    0x0A. C - argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,19 +2,13 @@
 #include "main.h"
 
 /**
-* atoi - converts a string to an integer
-
-* @s: string to be converted
-*
-
-* Return: the int converted from the string
-
-*/
-
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
 int _atoi(char *s)
-
 {
-
 int i, d, n, len, f, digit;
 
 i = 0;
@@ -23,12 +17,15 @@ n = 0;
 len = 0;
 f = 0;
 digit = 0;
+
 while (s[len] != '\0')
 len++;
+
 while (i < len && f == 0)
 {
 if (s[i] == '-')
 ++d;
+
 if (s[i] >= '0' && s[i] <= '9')
 {
 digit = s[i] - '0';
@@ -42,27 +39,24 @@ f = 0;
 }
 i++;
 }
+
 if (f == 0)
 return (0);
+
 return (n);
 }
+
 /**
-
-* main - multiplies two numbers
-
-* @argc: number of arguments
-
-* @argv: array of arguments
-
-*
-
-* Return: 0 (Success), 1 (Error)
-
-*/
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
 int main(int argc, char *argv[])
 {
-
 int result, num1, num2;
+
 if (argc < 3 || argc > 3)
 {
 printf("Error\n");
